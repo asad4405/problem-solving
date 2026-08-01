3c8d3303cbcd4afeae0dfb6a150efd6eb30d9d90
asad4405 <asadrifat450@gmail.com>
2026-08-01 18:41:29 +0600

    Custom-Problems: problem 1 complete

---
+++ Custom-Problems/problem-1.js
@@ -0,0 +1,11 @@
+// Problem 01: Check if a Number is Even or Odd
+
+function checkEvenOdd(number){
+    if (number % 2 === 0) { 
+        return "Even Number";
+    } else {
+        return "Odd Number";
+    }
+}
+
+// console.log(checkEvenOdd(4)); 
